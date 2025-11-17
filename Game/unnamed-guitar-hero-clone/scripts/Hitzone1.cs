99b7cb8c461499301e484f7d9a545958ef8e403d
Thomas Zegers <ps268011@summacollege.nl>
2025-11-17 15:10:40 +0100

    hitzone 1 leest uit settings bestabd

--- Game/unnamed-guitar-hero-clone/scripts/Hitzone1.cs
+++ Game/unnamed-guitar-hero-clone/scripts/Hitzone1.cs
@@ -3,9 +3,21 @@ using System;
 
 public partial class Hitzone1 : Hitzone
 {
+	private ConfigFile ConfigLocal = new();
+	
 	public override void _Ready()
 	{
-		HitKey = Key.A;
+		ConfigLocal.Load("res://settings.cfg");
+		var value = ConfigLocal.GetValue("Keybinds", "leftzone").ToString();
+		if (Enum.TryParse<Key>(value, out Key parsedKey))
+		{
+			HitKey = parsedKey;
+		}
+		else
+		{
+			GD.PrintErr($"Invalid key in config: {value}. Using default A.");
+			HitKey = Key.A;
+		}
 		base._Ready();
 	}
 }
