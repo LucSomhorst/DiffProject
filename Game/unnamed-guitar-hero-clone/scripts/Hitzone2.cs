7700ee39215e0e05927f5aee543e22e3566ce070
Thomas Zegers <ps268011@summacollege.nl>
2025-11-19 13:49:48 +0100

    fix

--- Game/unnamed-guitar-hero-clone/scripts/Hitzone2.cs
+++ Game/unnamed-guitar-hero-clone/scripts/Hitzone2.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Hitzone2 : Hitzone
 {
+	private ConfigFile ConfigLocal = new();
+	
 	public override void _Ready()
 	{
 		ConfigLocal.Load("res://settings.cfg");
