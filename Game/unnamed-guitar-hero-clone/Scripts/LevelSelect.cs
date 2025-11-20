17abb959cc5f469a3af446decaf70f2296dfcf9e
LucSomhorst <ps265419@edu.summacollege.nl>
2025-11-20 11:04:41 +0100

    basic levels auto generation complete

--- Game/unnamed-guitar-hero-clone/Scripts/LevelSelect.cs
+++ Game/unnamed-guitar-hero-clone/Scripts/LevelSelect.cs
@@ -1,39 +1,73 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
 using UnnamedGuitarHeroClone.Scripts;
+using FileAccess = Godot.FileAccess;
 
 public partial class LevelSelect : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	[Signal]
-	public delegate void Level1EventHandler();
-	[Signal]
-	public delegate void Level2EventHandler();
-	[Signal]
-	public delegate void Level3EventHandler();
 	[Signal]
 	public delegate void ExitBtnEventHandler();
+	
+	[Signal]
+	public delegate void StartLevelEventHandler(string levelPath);
 	public override void _Ready()
 	{
-		
+		LoadLevels();
 	}
 	
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	
-	private void OnLevel1Click()
+	public static string[] LoadResources(string path)
 	{
-		EmitSignalLevel1();
+		DirAccess dir_access = DirAccess.Open(path);
+		if (dir_access == null) { return null; }
+
+		string[] files = dir_access.GetFiles();
+		if (files == null) { return null; }
+
+		return files;
 	}
-	private void OnLevel2Click()
+	private void LoadLevels()
 	{
-		EmitSignalLevel2();
+		var levelBox = GetNode("%LevelBox");
+		var filesToLoad = LoadResources("res://Levels/");
+		var newfiles = FileAccess.Open("Users://Levels", FileAccess.ModeFlags.WriteRead);
+		for (int i = filesToLoad.Length; i < 10; i++)
+		{
+			string number = "error";
+			var levelnumber = i + 1;
+			if (levelnumber < 10)
+			{
+				number = "0" + levelnumber;
+			}
+			else
+			{
+				number  = "" + levelnumber;
+			}
+			var file = FileAccess.Open("res://Levels/Level " + number + ".json", FileAccess.ModeFlags.Write);
+			file.StoreLine(i.ToString());
+			Random rnd = new Random();
+			file.StoreLine(rnd.Next(1,5).ToString());
+		}
+		foreach (var file in filesToLoad)
+		{
+			var packedScene = ResourceLoader.Load<PackedScene>("res://Scenes/LevelButton.tscn");
+			var NodeScene = (LevelButton)packedScene.Instantiate();
+			
+			NodeScene.Constructor(file.Replace(".json",""),file);
+			NodeScene.LevelButtonClicked += LevelSelected;
+			levelBox.AddChild(NodeScene);
+		}
 	}
-	private void OnLevel3Click()
+
+	private void LevelSelected(string levelPath)
 	{
-		EmitSignalLevel3();
+		EmitSignalStartLevel(levelPath);
+		GD.Print("Level Selected");
 	}
 
 	private void OnExitBtnPressed()
